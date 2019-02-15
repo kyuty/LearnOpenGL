91d93307114825dbfab1bfed1049c2150e61b3ce
wangdong <240304011@qq.com>
2019-02-15 14:06:42 +0800

    Add old Utils
    Signed-off-by: wangdong <240304011@qq.com>

--- src/2.lighting/0.24_colors_scene/24_colors_scene.cpp
+++ src/2.lighting/0.24_colors_scene/24_colors_scene.cpp
@@ -19,8 +19,8 @@
 #include <glm/gtc/type_ptr.hpp>
 
 // Other includes
-#include "Shader.h"
-#include "Camera.h"
+#include "Utils/Shader.h"
+#include "Utils/Camera.h"
 
 
 // Function prototypes
@@ -55,6 +55,10 @@ int main()
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+#ifdef __APPLE__
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); 			// uncomment this statement to fix compilation on OS X
+#endif
 
 	// Create a GLFWwindow object that we can use for GLFW's functions
 	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "LearnOpenGL", nullptr, nullptr);
